a0d6141c3a6844a0da1d87aa985357ef17c992c9
Daniel Ureña <67337592+dlurena24@users.noreply.github.com>
2024-08-26 19:12:03 -0600

    Update ALU_tb.sv

--- Ejercicio1/ALU_tb.sv
+++ Ejercicio1/ALU_tb.sv
@@ -8,88 +8,88 @@ module ALU_tb();
 	//SUMA
 	a = 4'b0000;
 	b = 4'b0000;
-	opCode = 4'b0000;
+	opCode = 4'b1111;
 	#40
 	a = 4'b1111;
 	b = 4'b1111;
-	opCode = 4'b0000;
+	opCode = 4'b1111;
 	#40
 	//RESTA
 	a = 4'b0010;
 	b = 4'b0001;
-	opCode = 4'b0001;
+	opCode = 4'b1110;
 	#40
 	a = 4'b0000;
 	b = 4'b0001;
-	opCode = 4'b0001;
+	opCode = 4'b1110;
 	#40	
 	//MULTIPLICACION
 	a = 4'b0010;
 	b = 4'b0010;
-	opCode = 4'b0010;
+	opCode = 4'b1101;
 	#40
 	a = 4'b1111;
 	b = 4'b1111;
-	opCode = 4'b0010;
+	opCode = 4'b1101;
 	#40		
 	//DIVISION
 	a = 4'b0010;
 	b = 4'b0000;
-	opCode = 4'b0011;
+	opCode = 4'b1100;
 	#40
 	a = 4'b0100;
 	b = 4'b0010;
-	opCode = 4'b0011;
+	opCode = 4'b1100;
 	#40		
 	//MODULO
 	a = 4'b0010;
 	b = 4'b0001;
-	opCode = 4'b0100;
+	opCode = 4'b1011;
 	#40
 	a = 4'b0100;
 	b = 4'b0010;
-	opCode = 4'b0100;
+	opCode = 4'b1011;
 	#40
 	//SHIFTLEFT
 	a = 4'b1010;
-	opCode = 4'b0101;
+	opCode = 4'b1010;
 	#40
 	a = 4'b0101;
-	opCode = 4'b0101;
+	opCode = 4'b1010;
 	#40	
 	//SHIFTRIGHT
 	a = 4'b0010;
-	opCode = 4'b0110;
+	opCode = 4'b1001;
 	#40
 	a = 4'b0101;
-	opCode = 4'b0110;
+	opCode = 4'b1001;
 	#40	
 	//AND
-	a = 4'b0001;
-	b = 4'b0001;
-	opCode = 4'b0111;
-	#40
-	a = 4'b0001;
-	b = 4'b0000;
-	opCode = 4'b0111;
-	#40			
-	//OR
 	a = 4'b0001;
 	b = 4'b0001;
 	opCode = 4'b1000;
 	#40
 	a = 4'b0001;
 	b = 4'b0000;
-	opCode = 4'b1000;	
+	opCode = 4'b1000;
+	#40			
+	//OR
+	a = 4'b0001;
+	b = 4'b0001;
+	opCode = 4'b0111;
+	#40
+	a = 4'b0001;
+	b = 4'b0000;
+	opCode = 4'b0111;	
 	#40
 	//XOR
 	a = 4'b0001;
 	b = 4'b0001;
-	opCode = 4'b1001;
+	opCode = 4'b0110;
 	#40
 	a = 4'b0001;
 	b = 4'b0000;
-	opCode = 4'b1001;
+	opCode = 4'b0110;
 	
 	end
 endmodule
